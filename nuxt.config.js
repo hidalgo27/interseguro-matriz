aebed4ad2a144999b0b6dd92860aca6522676a98
Hidalgo <hidalgochponce@gmail.com>
2022-12-26 19:11:17 -0500

    Reto interseguro - rotar matriz NxN -90 grados antihorario

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,8 +28,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/persistedState.client.js', ssr: false },
-    { src: '~/plugins/vue-datepicker', ssr: false }
+
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
